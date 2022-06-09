27f011db297fe325ca345ec870a74957cf56296e
Malte Hientzsch <46093823+maltehie@users.noreply.github.com>
2022-06-09 19:05:53 +0200

    Enable register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
 import { SteamBackendServiceService } from '../services/steam-backend-service.service';
+import { SubmissionType } from './submissiontype';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +13,11 @@ export class LoginComponent implements OnInit {
   loginStatus = false;
   valueChanged = false;
   infoText: String;
+  submissionType = SubmissionType;
 
   loginForm = this.formBuilder.group({
-    username: ['', Validators.required],
-    password: ['', Validators.required]
+    username: [null, Validators.required],
+    password: [null, Validators.required]
   });
 
   constructor(private formBuilder: FormBuilder,
@@ -33,14 +34,41 @@ export class LoginComponent implements OnInit {
     
   }
 
-  onSubmit(): void {
+  onSubmit(st: SubmissionType): void {
+    switch(st){
+      case SubmissionType.login:
+        this.login();
+        break;
+      case SubmissionType.register:
+        this.register();
+        break;
+    }
+  }
+
+  login(): void{
     let result = this.steamBackendService.getData('user/'+this.loginForm.value.username+'/'+this.loginForm.value.password);
-    result.subscribe((data: any) => {
+    result.subscribe(data => {
       console.log('Response: ',data);
       this.loginStatus = data;
       this.valueChanged = true;
       this.infoText = this.loginStatus? 'Logged in successfully!' : 'Username and/ or Password wrong!';
     });
+  }
+
+  register(): void{
+    console.log('Fired register function');
+    let result = this.steamBackendService.postData('user/'+this.loginForm.value.username+'/'+this.loginForm.value.password, null);
+    result.subscribe((data) => {
+      this.valueChanged = true;
+      this.loginStatus = true;
+      this.infoText = 'User created successfully!'
+    },
+    (error) => {
+      console.log(error.status);
+      this.valueChanged = true;
+      this.loginStatus = false;
+      this.infoText = 'Username taken!'
+    })
   }
 
 }
